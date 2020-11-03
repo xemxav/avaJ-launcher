4557d7fc1a6e741850c1e7b7d91496d45c865bd2
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-11-03 20:37:11 +0100

    exception on Aircraft instantiation

--- AircraftFactory.java
+++ AircraftFactory.java
@@ -1,17 +1,29 @@
+class IncorrectAircraftTypeValue extends IllegalArgumentException {
+    IncorrectAircraftTypeValue(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
 class AircraftFactory {
 
     public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
         Flyable product = null;
-        switch (type) {
-            case "Baloon":
-                product = new Baloon(name, new Coordinates(longitude, latitude, height));
-                break;
-            case "Helicopter":
-                product = new Helicopter(name, new Coordinates(longitude, latitude, height));
-                break;
-            case "JetPlane":
-                product = new JetPlane(name, new Coordinates(longitude, latitude, height));
-                break;
+        try {
+            switch (type) {
+                case "Baloon":
+                    product = new Baloon(name, new Coordinates(longitude, latitude, height));
+                    break;
+                case "Helicopter":
+                    product = new Helicopter(name, new Coordinates(longitude, latitude, height));
+                    break;
+                case "JetPlane":
+                    product = new JetPlane(name, new Coordinates(longitude, latitude, height));
+                    break;
+                default:
+                    throw new IncorrectAircraftTypeValue(type +": is not a proper Aicraft type");
+            }
+        } catch (IncorrectCoordinateValue e) {
+            System.exit(-2);
         }
         return product;
     }
