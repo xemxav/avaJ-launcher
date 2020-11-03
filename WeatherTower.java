f8c7dcf34f8f55a365abe1242119a92a43914a81
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-11-03 20:38:17 +0100

    cleared comments

--- WeatherTower.java
+++ WeatherTower.java
@@ -1,12 +1,10 @@
 public class WeatherTower extends Tower {
 
-    //getweather
     public String getWeather(Coordinates coordinates) {
         WeatherProvider weatherProvider = WeatherProvider.getProvider();
         return weatherProvider.getCurrentWeather(coordinates);
     }
 
-    //set weather
     private void changeWeather() {
         this.conditionsChanged();
     }
