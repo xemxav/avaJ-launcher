884f749ebe86c4ff2f0cb8b538e1b57be3be1aa9
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-11-03 20:36:32 +0100

    exception on coordinates instantiation

--- Coordinates.java
+++ Coordinates.java
@@ -1,16 +1,23 @@
+class IncorrectCoordinateValue extends IllegalArgumentException {
+    IncorrectCoordinateValue(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
 class Coordinates {
     private int longitude;
     private int latitude;
     private int height;
 
     Coordinates(int longitude, int latitude,int height) {
-
-        if ((longitude < 0 || latitude < 0) || (height < 0 || height > 100)) {
-            System.out.println("Coordinates are not valid");
-//            throw Exception;
+        if (longitude <= 0 || latitude <= 0 || height <= 0) {
+            throw new IncorrectCoordinateValue("The coordinate value are not good");
         }
         this.longitude = longitude;
         this.latitude = latitude;
+        if (height >= 100) {
+            height = 100;
+        }
         this.height = height;
     }
 
@@ -23,4 +30,5 @@ class Coordinates {
     public int getHeight() {
         return height;
     }
+
 }
