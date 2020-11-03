d8f3f51cb6b3d90bd92cd548751a95c198b0865f
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-11-03 14:47:30 +0100

    Coordinates

--- Coordinates.java
+++ Coordinates.java
@@ -1,17 +1,20 @@
-public class Coordinates {
-    int longitude;
-    int latitude;
-    int height;
+class Coordinates {
+    private int longitude;
+    private int latitude;
+    private int height;
 
-    public Coordinates(int longitude, int latitude, int height) {
+    Coordinates(int longitude, int latitude,int height) {
+
+        if ((longitude < 0 || latitude < 0) || (height < 0 || height > 100)) {
+            System.out.println("Coordinates are not valid");
+//            throw Exception;
+        }
         this.longitude = longitude;
         this.latitude = latitude;
         this.height = height;
     }
 
-    public int getLongitude() {
-        return (longitude);
-    }
+    public int getLongitude() { return longitude; }
 
     public int getLatitude() {
         return latitude;
