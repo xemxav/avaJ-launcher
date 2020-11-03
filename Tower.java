b1d9c4b4d1e4fdd58ff8b82a6ec763b27ba07243
xemxav <xavier.emmanuel.moreau@gmail.com>
2020-11-03 17:30:31 +0100

    register/unregister tower

--- Tower.java
+++ Tower.java
@@ -4,11 +4,21 @@ abstract class Tower {
     private ArrayList<Flyable> observers = new ArrayList<>();
 
     public void register(Flyable flyable) {
-        observers.add(flyable);
+        if (!observers.contains(flyable)) {
+            observers.add(flyable);
+            System.out.println(String.format("Tower says: %s registered to weather tower"));
+        } else {
+            System.out.println(String.format("%s: already registered", flyable));
+        }
     }
 
     public void unregister(Flyable flyable) {
-        observers.remove(flyable);
+        if (observers.contains(flyable)) {
+            observers.remove(flyable);
+            System.out.println(String.format("Tower says: %s unregistered from weather tower"));
+        } else {
+            System.out.println(String.format("%s: is not registered yet", flyable));
+        }
     }
 
     protected void conditionsChanged() {
